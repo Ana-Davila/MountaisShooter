77167803b5b3df59eb1436b6ce7e5218ce8d0a09
Ana Davila Kulek <adkulek03@gmail.com>
2026-06-21 16:41:16 -0300

    Creating menu interations - Creating ABC class - Creating Factory - Level 1 Bg implementation: parallax

--- code/entity.py
+++ code/entity.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from abc import ABC,abstractmethod
+import pygame
 
-class Entity:
-    def __init__(self):
-        self.name = None
-        self.surf = None
-        self.rect = None
+class Entity(ABC):
+    def __init__(self, name: str, positon: tuple):
+        self.name = name
+        self.surf = pygame.image.load('./asset/' + name + '.png')
+        self.rect = self.surf.get_rect(left=positon[0], top=positon[1])
+        self.speed = 0
 
+    @abstractmethod
     def move(self, ):
         pass
